2c6df4847c10cb644b13272d2c7a255590b4285b
OliverGN <129585487+OliverGN@users.noreply.github.com>
2023-09-20 15:19:27 +1200

    Create website with basic CSS
    Create personal website with initial CSS, animation, and some placeholder links

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// Add the "show" class to the div after a delay (e.g., 1 second)
+setTimeout(function () {
+    document.querySelector('.fade-in').classList.add('show');
+}, 200); // 1000 milliseconds = 1 second
