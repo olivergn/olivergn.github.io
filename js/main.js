551a690d4707bbcdb2f47c5ba7913604d7ebeb35
Oliver Nyland <ognyland@gmail.com>
2024-08-13 20:23:09 +1200

    Remove unnecessary CSS and JS comments

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
-// Add the "show" class to the div after a delay (e.g., 1 second)
 setTimeout(function () {
     document.querySelectorAll('.fade-in').forEach(function(element) {
         element.classList.add('show');
     });
-}, 200); // 1000 milliseconds = 1 second
+}, 200);
